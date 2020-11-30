d94ccf0a1381a848a40ae4fe82f96ffecbd7f9b3
Akshay P <akshay.py@gmail.com>
2020-11-30 21:44:29 +0530

    kubernetes

--- spring-boot-jar/src/main/java/com/sapiens/controller/HelloController.java
+++ spring-boot-jar/src/main/java/com/sapiens/controller/HelloController.java
@@ -1,13 +1,22 @@
 package com.sapiens.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+	@Autowired
+	private Environment env;
 
 	@GetMapping("/helloDocker")
 	public String sayHello() {
-		return "Hello, Docker";
+		String developer = env.getProperty("app_dev");
+		if (!StringUtils.hasText(developer)) {
+			developer = "Default Dev";
+		}
+		return "Hello, Docker. This is developed by " + developer;
 	}
 }
